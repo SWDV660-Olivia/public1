c692b1ede81e0a8524d25b51cc31685815bf73b6
Olivia <osalvini1@live.maryville.edu>
2023-10-01 23:22:33 -0700

    Trying again...

--- webapplication2.js
+++ webapplication2.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Never give up on the dreams you hold dear.\n Keep doing your best and new paths will appear.\n Remember persistence; it goes with success.\n like a king with a queen in the great game of chess.')
+   response.end('Never give up on the dreams you hold dear.\n Keep doing your best and new paths will appear.\n Remember persistence; it goes with success.\n like a King with a Queen in the great game of chess.')
 }).listen(7777)
 
 // Console will print the message

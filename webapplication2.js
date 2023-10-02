7fe21bc8a41d7bacae719a24d5dae4875fc1dd87
Olivia <osalvini1@live.maryville.edu>
2023-10-01 22:28:43 -0700

    initial

---
+++ webapplication2.js
@@ -0,0 +1,15 @@
+var http = require("http")
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'})
+
+   // Send the response body as "Hello World"
+   response.end('Never give up on the dreams you hold dear.\n Keep doing your best and new paths will appear.\n Remember persistence; it goes with success.\n like a king with a queen in the great game of chess.')
+}).listen(7777)
+
+// Console will print the message
+console.log('Server running')
